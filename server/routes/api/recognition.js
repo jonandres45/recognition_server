c178967d8eba7868e35178d30d245cd0e906409a
jonandres45 <ansa1315@outlook.es>
2021-12-15 20:18:25 -0600

    Funciones y variables locales

--- server/routes/api/recognition.js
+++ server/routes/api/recognition.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const RecognitionController = require('../../controller/recognition.controller');
 
-router.get('/', RecognitionController.sendImage);
+router
+    .get('/description', RecognitionController.sendAndGetDescription)
+    .get('/labels', RecognitionController.sendAndGetLabels)
 
 module.exports = router;
