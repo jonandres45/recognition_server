49f8adef25ba9447af144de3d0bfba95c2a1aded
jonandres45 <ansa1315@outlook.es>
2021-12-23 00:34:34 -0600

    creacion de carpeta storage en caso de que no exista

--- server/lib/storage.js
+++ server/lib/storage.js
@@ -1,7 +1,11 @@
 const multer = require('multer');
-
+const fs = require('fs');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
+        if(!fs.existsSync('./storage')){
+            fs.mkdirSync('./storage',{recursive:true});
+            console.log("Carpeta creada");
+        }
         cb(null, './storage')
     },
     filename: function (req, file, cb) {
